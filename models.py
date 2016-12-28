3855ce751545920eb5f016d03d87e56b500295d0
thewellington00 <thewellington00@users.noreply.github.com>
2016-12-28 07:47:53 -0800

    capitalized Messages in models.py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from app import app
 
 db = SQLAlchemy(app)
 
-class messages(db.Model):
+class Messages(db.Model):
     __tablename__ = "messages"
     id = db.Column('id', db.Integer, primary_key=True)
     message = db.Column('message', db.Unicode)
