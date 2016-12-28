9564615530b97a7fc159694a112550f8f7802880
thewellington00 <thewellington00@users.noreply.github.com>
2016-12-28 07:41:15 -0800

    added postgres config to app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 
 # initiate flask app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # import views
 from views import *
