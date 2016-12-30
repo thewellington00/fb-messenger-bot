001d530ee060047b8f9e0b08e5e3e9741104a63e
thewellington00 <thewellington00@users.noreply.github.com>
2016-12-29 16:11:21 -0800

    added to nextbus

--- nextbus.py
+++ nextbus.py
@@ -1,3 +1,28 @@
 
 import requests
 from lxml import etree
+
+
+#xml next bus url
+url = 'http://webservices.nextbus.com/service/publicXMLFeed'
+
+#set parameters
+# params = {'command': 'routeConfig', 'a': 'sf-muni', 'r': '30'}
+params = {'command': 'predictions', 'a': 'sf-muni', 'r': '30', 'stopId': '14114'}
+
+#set request headers, gzip according to API guidelines
+headers = {'Accept-Encoding' : 'gzip, deflate'}
+
+#make request
+r = requests.get(url, params=params, headers=headers)
+
+xml = etree.fromstring(r.content)
+predictions = xml.xpath('//prediction')
+
+arrive_times = 'Arrives in '
+for predict in predictions:
+    arrive_times = arrive_times + predict.get('minutes') + ', '
+
+print arrive_times[:-2] + ' minutes'
+
+
